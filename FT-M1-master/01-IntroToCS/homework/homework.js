f490b2985d98b695f429052a114bb9c2852379dc
marylaurab <mbetancourt@fi.uba.ar>
2022-08-03 09:41:58 -0400

    Done!

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -17,12 +17,28 @@ function BinarioADecimal(num) {
   } return suma;
 } /* Si quisiera ahorrar el paso de pensar como expresar el num al cual se va a elevar, ya que en esta
 ocasion fue facil hallarla pero no me funciona en caso de que se complique, ya que no puedo hacerlo
-'manual', podria revertir el array y ahi si puedo elevar a 2 por i como viene recorriendo en el for*/
+'manual', podria revertir el array y ahi si puedo elevar a 2 por i como viene recorriendo en el for. O hacerlo
+directo con el metodo .ToString()*/
 
 function DecimalABinario(num) {
   // tu codigo aca
-
-}
+  /*
+  .Recibo un dato de tipo Number
+  .Lo divido entre 2 y me quedo con su parte entera para seguir dividiendola hasta que valga 1
+  .A cada parte entera, le sacare el resto entre 2 y lo guardare
+  .Cada resto lo guardo en un array
+  .Le doy vuelta al array, y lo retorno como string 
+   */
+  var ArrBinario = [];
+  var division;
+  while (num >= 1) {
+    division = Math.floor(num/2);
+    ArrBinario.push(num%2);
+    num = division;
+  } return ArrBinario.reverse().join('')
+} /*Cada itearacion va modificando el valor de num, el cual debo de guardar para la proxima iteracion. Por eso al final de todo,
+antes de seguir la otra iteracion, le asigno a num el valor ultimo que tuvo la division, y asi se puede evaluar en la condicion
+del while en la proxima*/
 
 
 module.exports = {
